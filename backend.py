d4e628555f0671a01093cd2de86bf93e73187e29
zachd93 <zachdebolt1993@gmail.com>
2023-12-15 15:53:31 -0800

    DAy 33: Implement API and get data from API

---
+++ backend.py
@@ -0,0 +1,13 @@
+import requests
+API_KEY = "782d3273279dc0d74e1cfeb016e07e68"
+
+
+def get_data(place, forecast_days=None, kind=None):
+    url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
+    response = requests.get(url)
+    data = response.json()
+    return data
+
+
+if __name__ == "__main__":
+    print(get_data(place="Tokyo"))
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import plotly.express as px
+from backend import get_data
 
+
+print(api)
 st.title("Weather Forecast fof the Next Few Days")
 place = st.text_input("Place: ")
 days = st.slider("Forecast Days", min_value=1, max_value=5,
@@ -9,14 +12,7 @@ option = st.selectbox("Select data to view",
                       ("Temperature", "Sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
 
-
-def get_data(days):
-    dates = ["2022-25-10", "2022-26-10", "2022-27-10"]
-    temperatures = [10, 11, 15]
-    temperatures = [days * i for i in temperatures]
-    return dates, temperatures
-
-d, t = get_data(days)
+data = get_data(place, days, option)
 
 figure = px.line(x=d, y=t, labels={"x": "date", "y": "Temperature (C)"})
 st.plotly_chart(figure)
